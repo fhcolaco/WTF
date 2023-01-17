ff4e36609efd2ade49e221d252d32e23c321a830
TiagoLopes13 <tiagolopes8172@escolasdesatao.pt>
2023-01-17 18:38:35 +0000

    First content on homepage

--- ui/src/components/Content.js
+++ ui/src/components/Content.js
@@ -1,20 +1,85 @@
 import React from "react";
+import { useState } from "react";
+import { Combobox } from "@headlessui/react";
+
+const people = [
+  "Durward Reynolds",
+  "Kenton Towne",
+  "Therese Wunsch",
+  "Benedict Kessler",
+  "Katelyn Rohan",
+];
 
 export default function Content() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
     <>
-      <div className="mt-40 rounded bg-white/40">
-        <h1>
-          7gtyudijsokpel34rtgffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddv
-        </h1>
-        <p>ds</p>
-        <p>ds</p>
-        <p>dsx</p>
-        <p>as</p>
-        <p>as</p>
-        <p>sd</p>
-        <p>ds</p>
-        <p>sd</p>
+      <div className="inset-x-0 top-0 mt-64">
+        <div className="mb-10 text-center text-5xl text-white">
+          <h1>Descubra o seu próximo Hotel!</h1>
+        </div>
+        <form className="flex flex-row justify-center">
+          <Combobox
+            as="div"
+            value={selectedPerson}
+            onChange={setSelectedPerson}
+          >
+            <Combobox.Input
+              onChange={(event) => setQuery(event.target.value)}
+              className="rounded"
+            />
+            <Combobox.Options className="bg-white">
+              {filteredPeople.map((person) => (
+                <Combobox.Option key={person} value={person}>
+                  {person}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
+          <Combobox
+            as="div"
+            value={selectedPerson}
+            onChange={setSelectedPerson}
+          >
+            <Combobox.Input
+              onChange={(event) => setQuery(event.target.value)}
+              className="rounded"
+            />
+            <Combobox.Options className="bg-white">
+              {filteredPeople.map((person) => (
+                <Combobox.Option key={person} value={person}>
+                  {person}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
+          <Combobox
+            as="div"
+            value={selectedPerson}
+            onChange={setSelectedPerson}
+          >
+            <Combobox.Input
+              onChange={(event) => setQuery(event.target.value)}
+              className="rounded"
+            />
+            <Combobox.Options className="bg-white">
+              {filteredPeople.map((person) => (
+                <Combobox.Option key={person} value={person}>
+                  {person}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
+        </form>
       </div>
     </>
   );
--- ui/src/components/Layout_Front.js
+++ ui/src/components/Layout_Front.js
@@ -5,14 +5,13 @@ import { Outlet } from "react-router-dom";
 export default function Layout_Front() {
   return (
     <div>
-      <div>
-        <Header></Header>
+      <Header />
+      <div className="absolute inset-0 mt-40">
+        <div className="relative z-10 mx-10">
+          <Outlet />
+        </div>
       </div>
-
-      <div className="absolute z-10 mt-32">
-        <Outlet />
-      </div>
-      <div className="z-0">
+      <div className="-z-10">
         <div className="fixed inset-0 h-screen bg-gradient-to-t from-black bg-cover"></div>
         <div className=" h-screen bg-[url('https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg')] bg-cover bg-no-repeat"></div>
         <div className=" h-screen bg-[url('https://checkin.com.pt/wp-content/uploads/2020/10/Captura-de-ecra%CC%83-2020-10-27-a%CC%80s-09.43.24.png')] bg-cover bg-no-repeat"></div>
