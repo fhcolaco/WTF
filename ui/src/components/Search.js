d918c208fdbca391b2ac10e189bedfb8dd5257e2
TiagoLopes13 <tiagolopes8172@escolasdesatao.pt>
2023-02-03 19:14:55 +0000

    Search page

--- ui/src/components/Search.js
+++ ui/src/components/Search.js
@@ -1,17 +1,47 @@
 import { Combobox } from "@headlessui/react";
-import { StarIcon } from "@heroicons/react/24/outline";
+import {
+  StarIcon,
+  MagnifyingGlassIcon,
+  UsersIcon,
+  MapPinIcon,
+  CalendarIcon,
+} from "@heroicons/react/24/outline";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getHotel } from "../shared/hotelApi";
 import { getHotelCategory } from "../shared/hotel_categoryApi";
 import Loader from "../Loader";
+import { DateRange } from "react-date-range";
+import { format } from "date-fns";
 
 export default function Search() {
+  const [selectedLocation, setSelectedLocation] = useState([]);
+  const [query, setQuery] = useState("");
+  const [locationList, setLocationList] = useState([]);
   const [hotelSearch, setHotelSearch] = useState([0, 1]);
-  const [hotel, setHotel] = useState({});
+  const [hotel, setHotel] = useState([]);
   const [hotelCategory, setHotelCategory] = useState([]);
   const [loading, setloading] = useState(true);
+  const [openQuantityOptions, setOpenQuantityOptions] = useState(false);
+  const [quantityOptions, setQuantityOptions] = useState({
+    pessoas: 2,
+    quartos: 1,
+  });
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const filteredLocation =
+    query === ""
+      ? locationList
+      : locationList.filter((loc) => {
+          return loc.toLowerCase().includes(query.toLowerCase());
+        });
 
   useEffect(() => {
     getHotel().then((res) => {
@@ -29,6 +59,18 @@ export default function Search() {
     }
   }, [hotel, hotelCategory]);
 
+  const handleQuantityOption = (item, operacao) => {
+    setQuantityOptions((prev) => {
+      return {
+        ...prev,
+        [item]:
+          operacao === "mais"
+            ? quantityOptions[item] + 1
+            : quantityOptions[item] - 1,
+      };
+    });
+  };
+
   return (
     <>
       {loading ? (
@@ -36,15 +78,132 @@ export default function Search() {
           <Loader />
         </div>
       ) : (
-        <div className="flex w-full flex-row">
-          <div className="fixed mr-6 h-auto w-3/12 rounded-lg border border-gray-300 bg-white p-4">
+        <div className="flex">
+          <div className=" mr-6 h-full w-4/12 rounded-lg border border-gray-300 bg-white p-4">
             <p className="text-2xl font-bold">Filtrar</p>
-            <Combobox>
-              <Combobox.Input
-                className="mt-6 h-10 w-56 rounded border-white px-4 outline outline-1 outline-black"
-                placeholder={"cidade"}
-              ></Combobox.Input>
-            </Combobox>
+            <form className="mt-6">
+              <h6>Destino</h6>
+              <Combobox
+                as="div"
+                onChange={setSelectedLocation}
+                className="relative"
+                onClick={() => setOpenDate(false)}
+              >
+                <div className="absolute inset-y-0 left-0 mb-1 flex items-center pl-3">
+                  <MapPinIcon className="h-6 w-6 text-gray-500" />
+                </div>
+                <Combobox.Input
+                  id="city"
+                  name="city"
+                  onChange={(event) => setQuery(event.target.value)}
+                  className="block w-72 rounded border-none pl-10 outline outline-1 outline-black focus:ring-orange-500 "
+                  placeholder={"cidade"}
+                ></Combobox.Input>
+                <Combobox.Options className="absolute mt-1 w-56 rounded bg-white">
+                  {filteredLocation.map((location, index) => (
+                    <Combobox.Option
+                      key={index}
+                      value={location}
+                      className="pl-4 hover:rounded hover:bg-blue-100"
+                    >
+                      {location}
+                    </Combobox.Option>
+                  ))}
+                </Combobox.Options>
+              </Combobox>
+              <h6 className="mt-3">Check-in/Check-out</h6>
+              <button
+                type="button"
+                className={` flex h-10 w-72 flex-row rounded bg-white px-4 py-2 align-middle text-gray-500  hover:cursor-pointer ${
+                  openDate
+                    ? "ring-1 ring-orange-500"
+                    : "outline outline-1 outline-black"
+                }`}
+                onClick={() => setOpenDate(!openDate)}
+              >
+                <CalendarIcon className="mr-2 h-6 w-6" />
+                {`${format(date[0].startDate, "dd/MM/yyyy")} até ${format(
+                  date[0].endDate,
+                  "dd/MM/yyyy"
+                )}`}
+              </button>
+              {openDate && (
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item) => setDate([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={date}
+                  className="absolute top-36 z-40 mr-5"
+                />
+              )}
+              <h6 className="mt-3">Hóspedes/Quartos</h6>
+              <button
+                className={`flex h-10 w-72 rounded bg-white py-2 px-8 align-middle text-gray-500 hover:cursor-pointer ${
+                  openQuantityOptions
+                    ? "ring-1 ring-orange-500"
+                    : "outline outline-1 outline-black"
+                }`}
+                type="button"
+                onClick={() => setOpenQuantityOptions(!openQuantityOptions)}
+              >
+                <UsersIcon className="mr-2 h-5 w-5" />
+                {quantityOptions.pessoas} Pessoas {quantityOptions.quartos}{" "}
+                Quartos
+              </button>
+              {openQuantityOptions && (
+                <div className="absolute top-32 left-2/4 ml-24 mt-0.5 w-60 rounded bg-white p-2">
+                  <div className="m-2 flex justify-between">
+                    <p>Pessoas</p>
+                    <div className="flex items-center gap-5">
+                      <button
+                        className="h-6 w-6 border border-black hover:border-orange-500"
+                        type="button"
+                        onClick={() => handleQuantityOption("pessoas", "menos")}
+                      >
+                        -
+                      </button>
+                      <span>{quantityOptions.pessoas}</span>
+                      <button
+                        className="h-6 w-6 border border-black hover:border-orange-500"
+                        type="button"
+                        onClick={() => handleQuantityOption("pessoas", "mais")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className="m-2 flex justify-between">
+                    <p>Quartos</p>
+                    <div className="flex items-center gap-5">
+                      <button
+                        className="h-6 w-6 border border-black hover:border-orange-500"
+                        type="button"
+                        onClick={() => handleQuantityOption("quartos", "menos")}
+                      >
+                        -
+                      </button>
+                      <span>{quantityOptions.quartos}</span>
+                      <button
+                        className="h-6 w-6 border border-black hover:border-orange-500"
+                        type="button"
+                        onClick={() => handleQuantityOption("quartos", "mais")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              <div>
+                <button
+                  type="button"
+                  className="mt-6 w-full rounded-lg bg-orange-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-orange-600"
+                >
+                  Filtrar
+                </button>
+              </div>
+            </form>
           </div>
           <div className="w-full flex-col">
             {hotelSearch.map((index) => {
@@ -55,10 +214,10 @@ export default function Search() {
                     src="https://aquashowpark.com/wp-content/uploads/2022/03/Hotelae%CC%81rio-65-e1646846021885.jpg"
                     alt=""
                   />
-                  <div className="flex h-full w-full flex-col p-4">
+                  <div className="grid w-full flex-col p-4">
                     <div>
                       <h5 class="mb-3 text-2xl font-bold text-gray-900">
-                        {/* {hotel[index].name} */}
+                        {hotel[index].name}
                         Default Name
                       </h5>
                       <p>Hotel Category - Location</p>
@@ -68,6 +227,15 @@ export default function Search() {
                         <StarIcon className="stroke h-6 w-6 fill-yellow-300 stroke-none" />
                         <StarIcon className="stroke h-6 w-6 fill-yellow-300 stroke-none" />
                       </div>
+                    </div>
+                    <div className="py-3">
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                      </p>
                     </div>
                     <div className="inset-0 flex items-end">
                       <div class="flex w-full items-center">
