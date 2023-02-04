1bf75df0b7ace9d255246cd353c66ecc7e1fbb65
Silva <ritatsilva@deloitte.pt>
2023-02-04 22:43:13 +0000

    About

--- ui/src/components/About.js
+++ ui/src/components/About.js
@@ -7,8 +7,26 @@ export default function About() {
       <img
         src={warningTravelFreak}
         alt="Warning: Travel Freak on the way"
-        className="h-96"
+        className="mx-auto h-96"
       />
+     <br></br>
+     <div className="bg-orange-500 p-8">
+            <h1 className="text-center text-4xl font-medium text-white ">Sobre Nós</h1>
+            <div className="bg-white p-8 rounded-lg">
+              <h1 className="text-lg font-medium">Como surgimos?</h1>
+                <p className="text-lg">Aqui vai a história da empresa</p>
+              <h1 className="text-lg font-medium">Qual o nosso objetivo?</h1>
+                <p className="text-lg">Aqui vai a missão da empresa</p>
+              <h1 className="text-lg font-medium">Como surgimos?</h1>
+                <p className="text-lg">Aqui vai a visão da empresa</p>
+                <h2 className="text-lg font-medium">Equipe</h2>
+                <ul>
+                    <li className="text-lg">Membro 1</li>
+                    <li className="text-lg">Membro 2</li>
+                    <li className="text-lg">Membro 3</li>
+                </ul>
+            </div>
+        </div>
     </div>
   );
 }
