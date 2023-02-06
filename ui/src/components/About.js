24562794e6c8387560f301bd54572a85c40507f1
Silva <ritatsilva@deloitte.pt>
2023-02-06 18:10:15 +0000

    About Up

--- ui/src/components/About.js
+++ ui/src/components/About.js
@@ -5,9 +5,9 @@ export default function About() {
   return (
     <div>
         <img
-          src={warningTravelFreak}
-          alt="Warning: Tral Freak on the way"
-          className="ml-auto h-48 text-right"
+          src="https://wtf-backend.onrender.com/images/PatchesWTF.png"
+          alt="Warning: Travel Freak on the way"
+          className="mx-auto h-80"
         />
         <br></br>
 
@@ -43,11 +43,14 @@ export default function About() {
             </span>
           </summary>
 
-          <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-            molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-            voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-            earum similique!
+          <p class="mt-4 leading-relaxed text-gray-700 text-justify">
+          Nós, WTF, surgimos como resultado de um projeto universitário que tinha como objetivo criar um site de reservas de quartos simples e fácil de usar. 
+          A equipa envolvida no projeto, composta por estudantes apaixonados por viagens e novas experiências, decidiu tornar-nos uma plataforma intuitiva e
+          acessível para as pessoas encontrarem quartos inusitados em todo o mundo.
+
+          Assim nascemos nós, o WTF, um site de reservas de quartos fácil de usar e intuitivo, que permite às pessoas encontrar e reservar facilmente quartos
+          inusitados em todo o mundo. Desde então, temos vindo a crescer em popularidade e somos agora um dos principais sites de reservas de quartos do mundo,
+          oferecendo a conveniência de uma plataforma de reservas simples e eficiente.
           </p>
         </details>
 
@@ -77,7 +80,41 @@ export default function About() {
             </span>
           </summary>
 
-          <p class="mt-4 leading-relaxed text-gray-700">
+          <p class="mt-4 leading-relaxed text-gray-700 text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+            molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+            voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+            earum similique!
+          </p>
+        </details>
+
+        <details
+          class="group border-l-4 border-orange-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+        >
+          <summary class="flex items-center justify-between cursor-pointer">
+            <h2 class="text-lg font-medium text-gray-900">
+            Contatos
+            </h2>
+
+            <span
+              class="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="flex-shrink-0 w-5 h-5 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p class="mt-4 leading-relaxed text-gray-700 text-justify">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
             molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
             voluptate dicta quo officiis explicabo consequuntur distinctio corporis
