082e6560c83a80ca2980a9f095e1ff2dd797c083
fhcolaco <pv25013@alunos.estgv.ipv.pt>
2023-02-08 22:43:59 +0000

    qwertygfdf

--- ui/src/backend/components/BookingDetail.jsx
+++ ui/src/backend/components/BookingDetail.jsx
@@ -3,46 +3,38 @@ import { useParams, useNavigate } from "react-router-dom";
 import Loader from "../../Loader";
 import { getBookingById } from "../../shared/bookingApi";
 import { getHotel } from "../../shared/hotelApi";
-import { getRooms } from "../../shared/roomApi";
+import { getRoom } from "../../shared/roomApi";
 
-export default function BookingDetail() => {
-    const [room, setRoom] = useState(null);
-    const [hotel, setHotel] = useState(null);
-    const [booking, setBooking] = useState(null);
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(true);
+export default function BookingDetail() {
+  const [room, setRoom] = useState(null);
+  const [hotel, setHotel] = useState(null);
+  const [booking, setBooking] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getBookingById(id).then((data) => {
-            setBooking(data);
-        });
-        getRooms().then((data) => {
-            setRoom(data);
-        });
-        getHotels().then((data) => {
-            setHotel(data);
-        });
-    }, [id]);
+  useEffect(() => {
+    getBookingById(id).then((data) => {
+      setBooking(data);
+    });
+    getRoom().then((data) => {
+      setRoom(data);
+    });
+    getHotel().then((data) => {
+      setHotel(data);
+    });
+  }, [id]);
 
-    useEffect(() => {
-        if (booking && room && hotel) {
-            setLoading(false);
-        }
-    }, [booking, room, hotel]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setRoom({ ...room, [name]: value });
-      };
-
-
-
-    return (
-        <> {
-            loading ? <Loader /> : (<div></div>)
-
-        }
-        </>
-    );
+  useEffect(() => {
+    if (booking && room && hotel) {
+      setLoading(false);
     }
+  }, [booking, room, hotel]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRoom({ ...room, [name]: value });
+  };
+
+  return <> {loading ? <Loader /> : <div></div>}</>;
+}
